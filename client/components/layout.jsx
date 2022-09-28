ceece1a1fc8225092211b683d2fb0169dce889f9
root <root@PhilYoo.localdomain>
2022-09-28 16:26:11 -0700

    Initial commit

---
+++ client/components/layout.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./navbar"
+import Footer
+ from "./footer"
+const Layout = ({ children }) => {
+  return (
+    <>
+      <NavBar />
+      <main>{ children }</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
