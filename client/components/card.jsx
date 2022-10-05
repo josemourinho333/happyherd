6c77a4e8fd835931abc9b7760ee92ae23920f12d
root <root@PhilYoo.localdomain>
2022-10-04 19:57:35 -0700

    Add moment js

--- client/components/card.jsx
+++ client/components/card.jsx
@@ -1,18 +1,17 @@
 import styles from '../styles/Card.module.scss';
-import { capitalize } from '../helpers/capitalize';
 import parse from 'html-react-parser';
 import { HiOutlineArrowRight } from "react-icons/hi";
-
+import moment from 'moment';
 
 const Card = (props) => {
-  // console.log('dadddd', props); 
+  console.log('dadddd', props); 
 
   const parsed = parse(props.data.content).filter((item) => item.type === "p");
   // console.log('parsed', parsed);
 
 
   return (
-    <article className={`${styles.cardBlog} bg-white mx-2 shadow-xl`}>
+    <article className={`${styles.cardBlog} bg-white mx-2 shadow-lg`}>
       <img src={props.data.featuredImage?.node.sourceUrl ? props.data.featuredImage?.node.sourceUrl : `https://happyherd.org/wp-content/uploads/2020/06/lincoln-lola-e1624407341856.jpg`} alt="" className={`${styles.img}`}/>
       <div className={`${styles.content}`}>
         <h1 className="text-2xl">{props.data.title}</h1>
@@ -21,6 +20,7 @@ const Card = (props) => {
           Read More
           <HiOutlineArrowRight className="ml-2 mt-1.5"/>
         </a>
+        <p className="text-xs">{moment(props.data.date).format('dddd, MMM Do, YYYY')}</p>
       </div>
     </article>
   );
