aa41b9d3e43667307e51d78f42a2a9dd04e16f39
root <root@PhilYoo.localdomain>
2022-10-19 16:52:39 -0700

    Update section styling

--- client/components/whoweare.jsx
+++ client/components/whoweare.jsx
@@ -12,23 +12,21 @@ const WhoWeAre = ({ data }) => {
   });
 
   return (
-    <section className="who-we-are-container flex items-center">
-      <div className="content-container flex flex-col basis-1/2 text-black px-[1rem]">
-        {/* <h1 className="font-semibold text-3xl p-5">Who We Are</h1> */}
-        {/* <div className="h-[1px] w-[50%] bg-slate-800"></div> */}
+    <section className="who-we-are-container flex items-center text-neutral">
+      <div className="content-container flex flex-col basis-1/2 px-[1rem]">
         <h1 className="font-semibold text-5xl">
           The Happy Herd is a <strong>farm animal sanctuary</strong> for animals either abused or at risk.
         </h1>
-        <p className="font-light my-[2rem]">
+        <p className="my-[2rem]">
           On 4 wonderful acres, you can find the many animals we have rescued roaming freely. We have several goats, sheep, chickens, cows, pigs, turkeys, ducks, cats, a dog, and a wonderful friendly donkey! Each with their <strong>own identity and personality</strong>, you can find out more about them by clicking below. We became official in August 2018, when we became a registered charity.
         </p>
-        <button className="bg-black border border-black text-white rounded-full px-5 py-2 self-start hover:bg-white hover:border hover:text-black flex align-center">
+        <button className="btn btn-primary px-5 py-2 self-start flex align-center sm:btn-sm md:btn-md lg:btn-md">
           Rescued Animals
           <HiOutlineArrowRight className="ml-2 mt-1.5"/>
         </button>
       </div>
 
-      <div className={`${styles["context-imgs"]} basis-1/2`}>
+      <div className={`${styles["context-imgs"]} py-[3rem] px-[1rem] basis-1/2`}>
         {imgs}
       </div>
 
