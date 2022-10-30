8c651e6b56d752410f9422f62d31cdfae532ac5d
root <root@PhilYoo.localdomain>
2022-10-30 16:11:40 -0700

    Add colorways in list and move away from cards component for products'

--- client/pages/garthsgiftshop.jsx
+++ client/pages/garthsgiftshop.jsx
@@ -3,7 +3,6 @@ import Hero from '../components/hero';
 import useSWR from 'swr';
 import { request } from 'graphql-request';
 import Card from '../components/card';
-import { data } from 'autoprefixer';
 
 const fetcher = (query) => request(process.env.happyHerdApi, query)
   .then((data) => data)
@@ -53,22 +52,93 @@ const GarthsGiftShop = () => {
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
-  console.log('data', data.items.nodes);
-
   const itemCards = data.items.nodes.map((item) => {
+
+    let colorOptions;
+    const colors = item.colors.nodes;
+    if (colors.length > 0) {
+      colorOptions = colors.map((color, index) => {
+        let displayColor;
+        switch(color.name) {
+
+          case "Light Blue":
+            displayColor = "bg-blue-300";
+            break;
+          
+          case "Pink":
+            displayColor = "bg-pink-300";
+            break;
+          
+          case "Blue":
+            displayColor = "bg-blue-500";
+            break;
+
+          case "Green":
+            displayColor = "bg-green-600";
+            break;
+
+          case "Grey":
+            displayColor = "bg-gray-500";
+            break;
+
+          case "Red":
+            displayColor = "bg-red-500";
+            break;
+          
+          case "White":
+            displayColor = "bg-white border border-black";
+            break;
+
+          case "Black":
+            displayColor = "bg-black";
+            break;
+
+          case "Burgundy":
+            displayColor = "bg-rose-900";
+            break;
+
+          case "Charcoal":
+            displayColor = "bg-zinc-800";
+            break;
+
+          case "Navy":
+            displayColor = "bg-blue-900";
+            break;
+
+          case "Red/Grey":
+            displayColor = "bg-gradient-to-r from-red-500 to-gray-500";
+            break;
+
+          default:
+            displayColor = "bg-slate-700"
+        }
+        return (
+          <div className="avatar placeholder" key={index}>
+            <div className={`${displayColor} rounded-full w-3`}>
+              <span className="text-xs"></span>
+            </div>
+          </div>
+        )
+      })
+    } else {
+      colorOptions = <></>;
+    }
+
     return (
-      <Card 
-        key={item.id}
-        id={item.id}
-        items={true}
-        name={item.title}
-        content={item.content}
-        slug={item.slug}
-        price={item.prices.nodes}
-        sizes={item.sizes.nodes}
-        colors={item.colors.nodes}
-        img={item.featuredImage?.node.sourceUrl}
-      />
+        <div key={item.id} className="flex flex-col basis-1/4 cursor-pointer">
+          <a href={`/garthsgiftshop/${item.title}`}>
+            <div className="product-img">
+              <img src={item.featuredImage?.node.sourceUrl} alt={item.slug} className=" h-[400px] w-full object-cover"/>
+            </div>
+            <div className="flex flex-col gap-y-2 my-3">
+              <h2 className="text-md hover:font-semibold">{item.title}</h2>
+              <p className="font-bold text-xl">${item.prices.nodes[0].name}</p>
+              <div className="flex gap-1">
+                {colorOptions}
+              </div>
+            </div>
+          </a>
+        </div>
     )
   })
 
