1bb11fdf1fc35db190a4726979364f90870407d1
root <root@PhilYoo.localdomain>
2022-11-01 19:09:10 -0700

    Add header meta tags for mobile view

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,18 @@
 import '../styles/globals.scss'
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <title>Happy Herd</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
