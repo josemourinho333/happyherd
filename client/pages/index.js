07bafdb6e7f58839e25ac0e2b6dcbbd3e1033277
root <root@PhilYoo.localdomain>
2022-11-03 12:23:35 -0700

    Add mobile views

--- client/pages/index.js
+++ client/pages/index.js
@@ -11,7 +11,7 @@ function Home({ data }) {
       <Carousel />
       <WhoWeAre data={data}/>
       <HowToHelp />
-      <LatestNews />
+      {/* <LatestNews /> */}
       <Subscribe />
     </>
   )
