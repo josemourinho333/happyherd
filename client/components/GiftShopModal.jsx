fa2c0c4f2e3f476b6e4aa3c23824eee09c6e5dea
root <root@PhilYoo.localdomain>
2022-11-04 21:01:34 -0700

    Add giftshop contact form component, finish gift shop section

--- client/components/GiftShopModal.jsx
+++ client/components/GiftShopModal.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import ContactForm from './ContactForm';
 
 const GiftShopModal = ({ id, color, size, name }) => {
 
@@ -11,18 +12,22 @@ const GiftShopModal = ({ id, color, size, name }) => {
       ? "You must select size."
       : `I would like to order the ${name} with the ${color} color and size ${size}`
 
+  const confirmation =
+    !color && !size
+      ? <ContactForm id={id} disabled="btn-disabled" body="You must select a color and a size."/>
+      : !color && size
+      ? <ContactForm id={id} disabled="btn-disabled" body="You must select a color."/>
+      : color && !size
+      ? <ContactForm id={id} disabled="btn-disabled" body="You must select a size."/>
+      : <ContactForm id={id} disabled="" body={`I would like to order the ${size} ${name} in ${color}.`}/>
+
   return (
     <>
       <input type="checkbox" id={id} className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-        <label htmlFor={id} className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-          <h3 className="font-bold text-lg">Send email to Happy Herd about this item</h3>
-          <p className="py-4">{body}</p>
-          <div className="modal-action">
-            <button className="btn btn-primary sm:btn-sm md:btn-sm lg:btn-sm">Send Email</button>
-            {/* <label htmlFor={id} className="btn">Yay!</label> */}
-          </div>
+          <label htmlFor={id} className="btn btn-sm btn-circle absolute right-2 top-2 z-[100]">✕</label>
+          {confirmation}
         </div>
       </div>
     </>
