d83160d5c54aa18e7a5b798ccec23aceeff3841f
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-14 18:26:36 -0800

    update daymodal

--- client/components/DayModal.jsx
+++ client/components/DayModal.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const DayModal = ({clickedDay}) => {
+
   return (
     <>
       <input type="checkbox" id={clickedDay} className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
           <h3 className="font-bold text-lg">{clickedDay}</h3>
-          <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+          <p className="py-4">You've been selected for a chanceza to get one year of subscription to use Wikipedia for free!</p>
           <div className="modal-action">
             <label htmlFor={clickedDay} className="btn">Yay!</label>
           </div>
